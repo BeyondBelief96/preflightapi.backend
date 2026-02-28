9932b4eb265d0696ac62a983c62bf750c040c15c
Brandon Berisford <brandonberisford@gmail.com>
2026-02-27 22:46:42 -0500

    Remove duplicate function attributes in NavaidFunction
    Eliminated redundant [Function] and [ExponentialBackoffRetry] attributes from NavaidFunction.cs, ensuring only a single set decorates the Run method for clarity and maintainability.

--- PreflightApi.Azure.Functions/Functions/NavaidFunction.cs
+++ PreflightApi.Azure.Functions/Functions/NavaidFunction.cs
@@ -28,8 +28,6 @@ namespace PreflightApi.Azure.Functions.Functions
 
         [Function("NavaidFunction")]
         [ExponentialBackoffRetry(5, "00:00:30", "00:15:00")]
-        [Function("NavaidFunction")]
-        [ExponentialBackoffRetry(5, "00:00:30", "00:15:00")]
         public async Task Run([TimerTrigger("0 45 10 * * *")] TimerInfo myTimer, FunctionContext context)
         {
             _logger.LogInformation("Navaid Function executed at: {Time}", DateTime.UtcNow);
